1a2598a71c2e4f7b06d475eb05d10574a608c7cd
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2024-07-13 10:36:42 +0100

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# Puppet manifest to install Flask version 2.1.0 using pip3
-
-package { 'Flask':
+# pip installs flask version 2.1.0
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
 }
-
