3dc0f273d1c4b71f3dfc1ed4f00f18020f02f3f9
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-09-22 18:15:27 +0100

    Task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'kill_killmenow_process':
+  command => '/usr/bin/pkill killmenow',
+  onlyif  => '/bin/pgrep killmenow',  # Executes only if the process is running
+  path    => ['/bin', '/usr/bin'],    # Ensure correct paths for commands
 }
